2396052cf4a692e4f7c6c102e3516c22bae64f89
LiangXiang Shen <kj415j45@gmail.com>
2019-01-04 22:25:33 +0800

    Update DemoPlugin.php

--- DemoPlugin.php
+++ DemoPlugin.php
@@ -7,10 +7,12 @@ use kjBot\Framework\Event\MessageEvent;
 
 class DemoPlugin extends Plugin{
     public $handleDepth = 3; //捕获到最底层的事件
-    public function beforePostMessage($queue){}
+    public $handleQueue = true; //声明是否要捕获消息队列
 
+    public function beforePostMessage($queue){} //若声明不需要捕获消息队列可不实现本方法
     //此处以正常群聊消息举例
-    public function message_group_normal(MessageEvent $event): Message{
-        return sendBack('Hello, world!', $event);
+    public function message_group_normal(MessageEvent $event): ?Message{
+        if(strpos($event->getMsg(), '你好') return $event->sendBack('Hello, world!', $event);
+        else return NULL;
     }
 }
