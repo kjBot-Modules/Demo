0f546cd401411f9dab3645a3b9fa5790fac7c34f
LiangXiang Shen <kj415j45@gmail.com>
2019-01-04 21:56:32 +0800

    Update DemoPlugin.php

--- DemoPlugin.php
+++ DemoPlugin.php
@@ -3,14 +3,14 @@ namespace kjBotModule\kjBot_Dev\Demo;
 
 use kjBot\Framework\Plugin;
 use kjBot\Framework\Message;
-use kjBot\Framework\Event\BaseEvent;
+use kjBot\Framework\Event\MessageEvent;
 
 class DemoPlugin extends Plugin{
     public $handleDepth = 3; //捕获到最底层的事件
     public function beforePostMessage($queue){}
 
     //此处以正常群聊消息举例
-    public function message_group_normal(BaseEvent $event): Message{
+    public function message_group_normal(MessageEvent $event): Message{
         return sendBack('Hello, world!', $event);
     }
 }
