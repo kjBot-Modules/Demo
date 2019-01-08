7477039bd0dc22c459515ae50ead972fe0c6d344
LiangXiang Shen <kj415j45@gmail.com>
2019-01-08 11:01:13 +0800

    Update DemoPlugin.php

--- DemoPlugin.php
+++ DemoPlugin.php
@@ -12,7 +12,7 @@ class DemoPlugin extends Plugin{
     public function beforePostMessage(&$queue){} //若声明不需要捕获消息队列可不实现本方法
     //此处以正常群聊消息举例
     public function message_group_normal(MessageEvent $event): ?Message{
-        if(strpos($event->getMsg(), '你好') return $event->sendBack('Hello, world!');
+        if(false !== strpos($event->getMsg(), '你好')) return $event->sendBack('Hello, world!');
         else return NULL;
     }
 }
